23ba1c0115833b698308d8fa83a3a65daa04cb0f
Thomas Lo <thomasjinlo@gmail.com>
2016-08-21 12:08:08 -0700

    Convert constants into immutable structures

--- calendar.js
+++ calendar.js
@@ -1,16 +1,19 @@
+let List = Immutable.List;
+let Map = Immutable.Map;
+
 const Calendar = (() => {
   // constants
-  const DAYSOFWEEK = [
-    {sh: 'Su', lg: 'Sunday'},
-    {sh: 'Mo', lg: 'Monday'},
-    {sh: 'Tu', lg: 'Tuesday'},
-    {sh: 'We', lg: 'Wednesday'},
-    {sh: 'Th', lg: 'Thursday'},
-    {sh: 'Fr', lg: 'Friday'},
-    {sh: 'Sa', lg: 'Saturday'}
-  ];
+  const DAYSOFWEEK = List.of(
+    Map({sh: 'Su', lg: 'Sunday'}),
+    Map({sh: 'Mo', lg: 'Monday'}),
+    Map({sh: 'Tu', lg: 'Tuesday'}),
+    Map({sh: 'We', lg: 'Wednesday'}),
+    Map({sh: 'Th', lg: 'Thursday'}),
+    Map({sh: 'Fr', lg: 'Friday'}),
+    Map({sh: 'Sa', lg: 'Saturday'})
+  );
 
-  const MONTHS = [
+  const MONTHS = List.of(
     'January',
     'February',
     'March',
@@ -22,12 +25,12 @@ const Calendar = (() => {
     'September',
     'November',
     'December'
-  ];
+  );
 
   // pure functions to get data
   const numDay = date => date.getDate();
-  const wordDay = date => DAYSOFWEEK[date.getDay()]['lg'];
-  const month = date => MONTHS[date.getMonth()];
+  const wordDay = date => DAYSOFWEEK.getIn([date.getDay(), 'lg']);
+  const month = date => MONTHS.get(date.getMonth());
   const year = date => date.getFullYear();
   const firstDayOfMonth = date => new Date(date.getYear(), date.getMonth(), 1).getDay();
   const daysOfMonth = date => new Date(date.getYear(), date.getMonth() + 1, 0).getDate();
